89f3138da9004db4e4230d0f82da62da52de7d75
ChandiniShivanna <chandinigj@gmail.com>
2023-01-16 19:29:28 +0530

    node-aasignment-2

---
+++ readCommandLineArguments.js
@@ -0,0 +1,2 @@
+const user=process.argv[2]
+console.log("Hello "+ user);
---
+++ readFromLine.js
@@ -0,0 +1,8 @@
+const readline=require('readline');
+const r1=readline.createInterface({
+    input:process.stdin,
+    output:process.stdout});
+r1.question("Please Enter your name ",(ans)=>{
+    console.log("Hello "+ans);
+    r1.close();
+})
